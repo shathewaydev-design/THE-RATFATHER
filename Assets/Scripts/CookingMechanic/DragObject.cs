d37fb1b62ef5f2687b5e7a041363f2c6afa33ee5
Chan Dong Ha <158008421+charlie-ha@users.noreply.github.com>
2026-05-13 18:01:28 -0400

    drag object script may 13

--- Assets/Scripts/CookingMechanic/DragObject.cs
+++ Assets/Scripts/CookingMechanic/DragObject.cs
@@ -4,6 +4,15 @@ using StarterAssets;
 
 public class DragObject : MonoBehaviour
 {
+    //Note that in the future, if i want to drag specific object, i need to cache it
+    //then move/rotate it
+    //Click object
+    // ->Cache selected object->Begin dragging->
+    // Move ONLY selected object->Release mouse->Stop dragging
+    //if (Physics.Raycast(ray, out RaycastHit hit, 100f, draggableMask))
+    // {
+    //     selectedObject = hit.transform;
+    // }
     private ThirdPersonController controller;
     private Camera mainCamera;
 
@@ -17,6 +26,8 @@ public class DragObject : MonoBehaviour
     [SerializeField] private bool moveRotation = false;
     [SerializeField] private float maxRotation = 28f;
     [SerializeField] private float minRotation = 0f;
+
+    [SerializeField] private LayerMask draggableMask;
 
     private void Start()
     {
@@ -54,12 +65,17 @@ public class DragObject : MonoBehaviour
         }
         Ray ray = mainCamera.ScreenPointToRay(mousePos);
 
-        // Draws a red ray in Scene view
-        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
-        
         objectToDrag.layer = LayerMask.NameToLayer("Dragging");
+        if (movePosition)
+        {
+            MoveObject(ray);
+        }
+        else if (moveRotation)
+        {
+            RotateObject(ray);
+        }
 
-        Vector3 screenPoint = new Vector3(mousePos.x, mousePos.y, distanceFromCamera);
+        /*Vector3 screenPoint = new Vector3(mousePos.x, mousePos.y, distanceFromCamera);
         Vector3 worldPos = mainCamera.ScreenToWorldPoint(screenPoint);
         
         if (movePosition)
@@ -78,6 +94,33 @@ public class DragObject : MonoBehaviour
             {
                 angle = minRotation;
             }
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+        }*/
+    }
+    private void MoveObject(Ray ray)
+    {
+        Plane dragPlane = new Plane(Vector3.forward, transform.position);
+
+        if (dragPlane.Raycast(ray, out float distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+            transform.position = new Vector3(point.x, point.y, transform.position.z);
+        }
+    }
+    private void RotateObject(Ray ray)
+    {
+        Plane rotationPlane = new Plane(Vector3.forward, transform.position);
+
+        if (rotationPlane.Raycast(ray, out float distance))
+        {
+            Vector3 point = ray.GetPoint(distance);
+
+            Vector3 direction = point - transform.position;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            angle = Mathf.Clamp(angle, minRotation, maxRotation);
+
             transform.rotation = Quaternion.Euler(0, 0, angle);
         }
     }
